80d662b94deaf1abed0644d5178746bab2005fa5
Kyle Bradshaw <kyle.bradshaw@gmail.com>
2022-02-28 15:38:37 -0500

    solver unit tests for all challenge permutations

--- src/app/services/game.service.spec.ts
+++ src/app/services/game.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { GuessClass } from 'src/app/models/guess';
 import { StorageService } from 'src/app/services/storage.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { GameService } from './game.service';
 
@@ -8,6 +10,9 @@ describe('GameService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
       providers: [
         { provide: StorageService }
       ]
@@ -17,5 +22,50 @@ describe('GameService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('solver() algo', () => {
+    it('should solve frame/frame', () => {
+      expect(service.solver('frame', 'frame')).toEqual([GuessClass.MATCH, GuessClass.MATCH, GuessClass.MATCH, GuessClass.MATCH, GuessClass.MATCH]);
+    });
+    it('should solve retry/rebut', () => {
+      expect(service.solver('retry', 'rebut')).toEqual([GuessClass.MATCH, GuessClass.MATCH, GuessClass.MISMATCH, GuessClass.USED, GuessClass.USED]);
+    });
+    it('should solve mammy/mommy', () => {
+      expect(service.solver('mammy', 'mommy')).toEqual([GuessClass.MATCH, GuessClass.USED, GuessClass.MATCH, GuessClass.MATCH, GuessClass.MATCH]);
+    });
+    it('should solve plate/pleat', () => {
+      expect(service.solver('plate', 'pleat')).toEqual([GuessClass.MATCH, GuessClass.MATCH, GuessClass.MISMATCH, GuessClass.MISMATCH, GuessClass.MISMATCH]);
+    });
+    it('should solve bully/blurt', () => {
+      expect(service.solver('bully', 'blurt')).toEqual([GuessClass.MATCH, GuessClass.MISMATCH, GuessClass.MISMATCH, GuessClass.USED, GuessClass.USED]);
+    });
+    it('should solve terse/trend', () => {
+      expect(service.solver('terse', 'trend')).toEqual([GuessClass.MATCH, GuessClass.MISMATCH, GuessClass.MISMATCH, GuessClass.USED, GuessClass.USED]);
+    });
+    it('should solve stiff/swift', () => {
+      expect(service.solver('stiff', 'swift')).toEqual([GuessClass.MATCH, GuessClass.MISMATCH, GuessClass.MATCH, GuessClass.MATCH, GuessClass.USED]);
+    });
+    it('should solve swift/stiff', () => {
+      expect(service.solver('swift', 'stiff')).toEqual([GuessClass.MATCH, GuessClass.USED, GuessClass.MATCH, GuessClass.MATCH, GuessClass.MISMATCH]);
+    });
+    it('should solve piece/prize', () => {
+      expect(service.solver('piece', 'prize')).toEqual([GuessClass.MATCH, GuessClass.MISMATCH, GuessClass.USED, GuessClass.USED, GuessClass.MATCH]);
+    });
+    it('should solve evoke/bowel', () => {
+      expect(service.solver('evoke', 'bowel')).toEqual([GuessClass.MISMATCH, GuessClass.USED, GuessClass.MISMATCH, GuessClass.USED, GuessClass.USED]);
+    });
+    it('should solve tally/talon', () => {
+      expect(service.solver('tally', 'talon')).toEqual([GuessClass.MATCH, GuessClass.MATCH, GuessClass.MATCH, GuessClass.USED, GuessClass.USED]);
+    });
+    it('should solve apart/azure', () => {
+      expect(service.solver('apart', 'azure')).toEqual([GuessClass.MATCH, GuessClass.USED, GuessClass.USED, GuessClass.MATCH, GuessClass.USED]);
+    });
+    it('should solve woody/glove', () => {
+      expect(service.solver('woody', 'glove')).toEqual([GuessClass.USED, GuessClass.USED, GuessClass.MATCH, GuessClass.USED, GuessClass.USED]);
+    });
+    it('should solve attic/admit', () => {
+      expect(service.solver('attic', 'admit')).toEqual([GuessClass.MATCH, GuessClass.MISMATCH, GuessClass.USED, GuessClass.MATCH, GuessClass.USED]);
+    });
   });
 });
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,3 +1,4 @@
+import { WordService } from 'src/app/services/word.service';
 import { StorageKey, StorageService } from 'src/app/services/storage.service';
 import { Injectable } from '@angular/core';
 import { forceRefresh } from 'src/app/helpers';
@@ -5,6 +6,7 @@ import { GameMode, GameState } from 'src/app/models/game';
 import { isBefore, isAfter } from 'date-fns';
 // import {} from 'date-fns-tz';
 import { get } from 'lodash';
+import { GuessClass } from 'src/app/models/guess';
 
 export enum ProceedState {
   SequenceNew = 'sequenceNew',
@@ -20,6 +22,7 @@ export class GameService {
 
   constructor(
     private storageService: StorageService,
+    private wordService: WordService,
   ) { }
 
   /**
@@ -51,5 +54,48 @@ export class GameService {
     this.storageService.clear(true);
     this.storageService.set(StorageKey.GameMode, GameMode[gameMode]);
     forceRefresh(gameMode);
+  }
+
+  /**
+   * Validates a round's guess
+   * @param sequence - the string of the guess word
+   * @param decodeWord - the plaintext string of the solution word
+   * @returns an array of GuessClass for each letter in sequence against the decodeWord solution
+   */
+  solver(sequence: string, decodedWord: string): GuessClass[] {
+    const repeatedSequence = this.wordService.repeatedCharacters(sequence);
+
+    return [...sequence]
+      // 1st pass does the match and unused
+      .map((letter, idx) => {
+        if (decodedWord[idx] === letter) {
+          return GuessClass.MATCH;
+        } else if ([...decodedWord].includes(letter)) {
+          return GuessClass.MISMATCH;
+        } else {
+          return GuessClass.USED;
+        }
+      })
+      // 2nd pass figures out mismatch resolution
+      .map((guessClass: GuessClass, idx) => {
+        const letter = [...sequence][idx];
+        if (guessClass === GuessClass.MISMATCH && repeatedSequence.includes(letter)) {
+          // what are the indicies of the repeated letter
+          const indicies = [...sequence.matchAll(new RegExp(letter, 'g'))].map(match => match.index);
+          if (indicies.length === 2 && indicies[0] === idx) {
+            // only take the first index into consideration IF the second one is not a MATCH
+            if (decodedWord.lastIndexOf(letter) === indicies.pop()) {
+              return GuessClass.USED;
+            } else {
+              return GuessClass.MISMATCH;
+            }
+          } else if (indicies.length === 3 && (indicies[0] === idx || indicies[1] === idx)) {
+            // only take the first 2 indicies into consideration
+            return GuessClass.MISMATCH;
+          }
+          return GuessClass.USED;
+        }
+        return guessClass;
+      });
   }
 }
